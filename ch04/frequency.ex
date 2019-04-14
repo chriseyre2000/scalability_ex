830c1ea86f295ee45cbbab2a01945a0fdb905050
Christopher Eyre <chriseyre2000@gmail.com>
2019-04-14 14:30:25 +0100

    Fixed frequency

--- ch04/frequency.ex
+++ ch04/frequency.ex
@@ -1,12 +1,10 @@
 defmodule Frequency do
   @moduledoc """
-  This is a broken example.
+  This is now a working example
   """
   use GenServer
 
-  def start do
-    GenServer.start_link(__MODULE__, {:local, __MODULE__},  [])
-  end
+ 
 
   def init(_arg) do
     frequencies = {get_frequencies(), []}
@@ -18,11 +16,10 @@ defmodule Frequency do
     {:reply, reply, new_frequencies}
   end
 
-  defp get_frequencies, do: 10..15
+  defp get_frequencies, do: [10,11,12,13,14,15]
 
-  def allocate, do: GenServer.call(__MODULE__, {:allocate, self()})
 
- def allocate({[], allocated}, _pid), do: {{[], allocated}, {:error, :no_frequency}}
+  def allocate({[], allocated}, _pid), do: {{[], allocated}, {:error, :no_frequency}}
 
   def allocate({[freq|free], allocated}, pid), do: {{free, [{freq, pid} | allocated ]}, {:ok, freq}}
 
@@ -31,7 +28,7 @@ defmodule Frequency do
     {[freq|free], new_allocated}  
   end
 
-  def deallocate(frequency), do: GenServer.cast(:frequency, {:deallocate, frequency})
+  
 
   def handle_cast({:deallocate, freq}, frequencies) do
     new_frequencies = deallocate(frequencies, freq)
@@ -49,9 +46,18 @@ defmodule Frequency do
 
   def handle_info(_msg, loop_data), do: {:noreply, loop_data}
 
+  # Client API
+
+  def start do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__ )
+  end
+
   def stop, do: GenServer.cast(__MODULE__, :stop)
 
   def terminate(_reason, _loopdata), do: :ok
 
+  def allocate, do: GenServer.call(__MODULE__, {:allocate, self()})
+
+  def deallocate(frequency), do: GenServer.cast(:frequency, {:deallocate, frequency})
   
 end
